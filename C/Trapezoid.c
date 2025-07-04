328ffb6e31cabf32693bc42e47edd2930492b30c
EarthDaichi <pongsakornsinsuwan1@gmail.com>
2025-07-04 22:07:31 +0700

    Add files via upload

---
+++ C/Trapezoid.c
@@ -0,0 +1,13 @@
+#include "fullstd.h"
+
+int a,b,h;
+
+int main() {
+  printf("input a");
+  scanf("%d",&a);
+  printf("input b");
+  scanf("%d",&b);
+  printf("input h");
+  scanf("%d",&h);
+  printf("Area is %f\n", (h*(a+b)*0.5));
+}
