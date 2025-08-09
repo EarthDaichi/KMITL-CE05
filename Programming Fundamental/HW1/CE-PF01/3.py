2bf04ad72a89ebe18611429f91a8d2500599ea04
EarthDaichi <pongsakornsinsuwan1@gmail.com>
2025-08-10 01:26:09 +0700

    Delete Programming Fundamental/HW1/CE-PF01 directory

--- Programming Fundamental/HW1/CE-PF01/3.py
+++
@@ -1,9 +0,0 @@
-#Pongsakorn Sinsuwan 68200609 KMITL PCC CE05
-#Write and program to input Hours,Minutes and Seconds then arrange to HH:MM:SS
-Hr = str(input("Enter Hour : "))
-Min = str(input("Enter Minutes : "))
-Sec = str(input("Enter Seconds : "))
-if int(Hr) > 24 : print(f"Error Hour is more than 24")
-elif int(Min) > 60 : print(f"Error Minutes is more than 60")
-elif int(Sec) > 60 : print(f"Error Seconds is more than 60")
-else : print(f"{Hr}:{Min}:{Sec}")
